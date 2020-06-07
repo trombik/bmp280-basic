120cf7345c2a216fe377fe7f05461108b34c7446
Tomoyuki Sakurai <y@trombik.org>
2020-06-08 04:30:03 +0900

    bugfix: use BMP280_MODE_FORCED
    the default is not for esp_deep_sleep().
    
    fixes incorrect pressure value. also, declare variables at the beginning
    of function

--- main/main.c
+++ main/main.c
@@ -61,17 +61,29 @@ int WIFI_CONNECTED_BIT = BIT0;
 void bmp280_test(void *pvParamters)
 {
     bmp280_params_t params;
-    bmp280_init_default_params(&params);
     bmp280_t dev;
+    bool bme280p;
+    bool busy;
+    float pressure, temperature, humidity;
+
     memset(&dev, 0, sizeof(bmp280_t));
+    bmp280_init_default_params(&params);
+    params.mode = BMP280_MODE_FORCED;
 
     ESP_ERROR_CHECK(bmp280_init_desc(&dev, BMP280_I2C_ADDRESS_0, 0, SDA_GPIO, SCL_GPIO));
     ESP_ERROR_CHECK(bmp280_init(&dev, &params));
 
-    bool bme280p = dev.id == BME280_CHIP_ID;
+    bme280p = dev.id == BME280_CHIP_ID;
     printf("BMP280: found %s\n", bme280p ? "BME280" : "BMP280");
 
-    float pressure, temperature, humidity;
+    bmp280_force_measurement(&dev);
+    do {
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+        if (bmp280_is_measuring(&dev, &busy) != ESP_OK) {
+            ESP_LOGE(TAG, "Failed to bmp280_is_measuring()");
+            goto fail;
+        }
+    } while (busy);
 
     if (bmp280_read_float(&dev, &temperature, &pressure, &humidity) != ESP_OK)
     {
